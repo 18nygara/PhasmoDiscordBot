c8acd14af51f8083448bacce81918edd60d42021
18nygara <dragynmada@gmail.com>
2021-05-17 15:40:10 -0500

    Add random item command

---
+++ commands/items.js
@@ -0,0 +1,30 @@
+const item_list = ['Spirit Box',
+  'Ghost Writing Book',
+  'Photo Camera',
+  'EMF Reader',
+  'Video Camera',
+  'UV Flashlight',
+  'Flashlight',
+  'Candle',
+  'Crucifix',
+  'Glow Stick',
+  'Head Mounted Camera',
+  'Infrared Light Sensor',
+  'Lighter',
+  'Motion Sensor',
+  'Parabolic Microphone',
+  'Salt Shaker',
+  'Sanity Pills',
+  'Smudge Sticks',
+  'Sound Sensor',
+  'Strong Flashlight',
+  'Thermometer',
+  'Tripod'];
+
+module.exports = {
+    name: 'item',
+    description: 'Randomize an item from the available pool',
+    execute(message, args) {
+      message.channel.send(item_list[Math.floor(Math.random()*item_list.length)]);
+    },
+  };
