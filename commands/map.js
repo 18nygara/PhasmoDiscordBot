2df3011f00eae697bcfef1433df0b955b33cf77f
18nygara <dragynmada@gmail.com>
2021-09-13 18:38:01 -0500

    Fix map command

--- commands/map.js
+++ commands/map.js
@@ -2,13 +2,11 @@ const maps = ["Left Map", "Right Map", "Top Map", "Bottom Map", "Middle Map",
   "Your Choice", "Biggest Map", "Smallest Map"];
 
 module.exports = {
-	map_options: maps
   name: 'map',
   description: 'Pick a random map.',
   execute(message, args) {
-    if (!args.length) {
-      message.channel.send("\nMap: " + maps.map_options[Math.floor(Math.random()*maps.map_options.length)]);
-      return;
-    }
-  }
+    message.channel.send("\nMap: " + maps[Math.floor(Math.random()*maps.length)]);
+    return;
+  },
+  map_options: maps
 };
